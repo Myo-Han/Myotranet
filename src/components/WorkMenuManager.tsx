0076c1cf3dcf178f377c11ae5bb75ecb03a52e6a
leenaaq <v584731@gmail.com>
2025-12-11 09:43:22 +0900

    s

--- src/components/WorkMenuManager.tsx
+++ src/components/WorkMenuManager.tsx
@@ -238,7 +238,7 @@ const WorkMenuManager: React.FC = () => {
 
       {showModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white rounded-lg p-6 max-w-lg w-full mx-4">
+          <div className="bg-white rounded-lg p-6 max-w-lg w-full mx-4 max-h-[90vh] overflow-y-auto">
             <h3 className="text-xl font-semibold mb-4">
               {editingItem ? '메뉴 수정' : '메뉴 추가'}
             </h3>
